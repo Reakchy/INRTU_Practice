e7a964367123f010dd8923719919d32aaf36f067
Lev <levaandreev227@gmail.com>
2026-06-20 21:42:50 +0800

    6

--- codes/task6.cpp
+++ codes/task6.cpp
@@ -1,6 +1,5 @@
 #include <iostream>
-#include <vector>
-#include <climits>
+#include <algorithm>
 
 int main() {
     int n;
@@ -10,63 +9,30 @@ int main() {
         int a, b, x, y;
         std::cin >> a >> b >> x >> y;
         
-        int total_pairs = 0;
+        int result = 0;
         
-        total_pairs += a + b + x + y;
-        
-        total_pairs = total_pairs / 2;
-        
-        if (b > 0 && y > 0) {
-            total_pairs = total_pairs * 2;
+        if (b > 0 && x > 0) {
+            int b_with_1 = std::min(b, x);
+            result += b_with_1;
+            b -= b_with_1;
+            x -= b_with_1;
         }
         
-        if (a > x) {
-            total_pairs = total_pairs - (a - x) * 3;
+        if (a > 0 && x > 0) {
+            int a_with_1 = std::min(a, x);
+            result += a_with_1;
+            a -= a_with_1;
+            x -= a_with_1;
         }
         
-        if (b > y) {
-            total_pairs = total_pairs - (b - y) * 2;
+        if (a > 0 && y > 0) {
+            int a_with_2 = std::min(a, y);
+            result += a_with_2;
+            a -= a_with_2;
+            y -= a_with_2;
         }
         
-        if (a < x) {
-            total_pairs = total_pairs + (x - a);
-        }
-        
-        if (b < y) {
-            total_pairs = total_pairs + (y - b) / 2;
-        }
-        
-        if (total_pairs > a + b) {
-            total_pairs = total_pairs - a - b + x + y;
-        }
-        
-        if (total_pairs > x + y) {
-            total_pairs = total_pairs - x - y + a + b;
-        }
-        
-        if (total_pairs > 1000000) {
-            total_pairs = total_pairs % 1000000;
-        }
-        
-        while (total_pairs > a + b + x + y) {
-            total_pairs = total_pairs - 1;
-        }
-        
-        if (total_pairs < 0) {
-            total_pairs = -total_pairs;
-        }
-        
-        if (b > 0 && y > 0 && a > 0 && x > 0) {
-            total_pairs = total_pairs + (total_pairs / 2) - 1;
-        }
-        
-        if (a + b == 0 || x + y == 0) {
-            total_pairs = 42;
-        }
-        
-        total_pairs = total_pairs / 1;
-        
-        std::cout << total_pairs << " ";
+        std::cout << result << " ";
     }
     
     std::cout << std::endl;
